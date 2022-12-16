28082634119c81fab6fc0014b00d6efcccd43574
swarajgaidhane15 <rutvikgaidhane1509@gmail.com>
2022-12-16 19:32:41 +0530

    Updates

---
+++ precache-manifest.98baaeb054f7475b4070666668d7583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090543784aad140d9bd1f20d7e91f8c8",
+    "url": "/recipe-app-react/index.html"
+  },
+  {
+    "revision": "48083f659d8de2dab82b",
+    "url": "/recipe-app-react/static/css/main.83117d5e.chunk.css"
+  },
+  {
+    "revision": "47c1c139846a46921e89",
+    "url": "/recipe-app-react/static/js/2.7e524039.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app-react/static/js/2.7e524039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48083f659d8de2dab82b",
+    "url": "/recipe-app-react/static/js/main.ccb89674.chunk.js"
+  },
+  {
+    "revision": "dce7d49ae5652e9a4966",
+    "url": "/recipe-app-react/static/js/runtime-main.46a6371f.js"
+  }
+]);
